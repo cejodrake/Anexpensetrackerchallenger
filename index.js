79f2adc4e38ab0e7a9e959b61162bfd7caf58d24
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-04 21:55:04 -0700

    Configuration for api test categories

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb://localhost/expensetrackerdb', { useNewUrlParser: true });
+
 
 app.use(express.json());
 app.use('/api/categories', categories);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
