d07c8f78c79f3a2c43b93d7d3059ebf06e3b9b07
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-04 21:33:19 -0700

    app.listen(port, () => console.log(`Listening on port ${port}...`));

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Joi = require('joi');
 const categories = require('./routes/categories');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/expensetrackerdb', { useNewUrlParser: true });
 
 app.use(express.json());
 app.use('/api/categories', categories);
