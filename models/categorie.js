12ee65fa7a2156b5a68d0217df392ef2b7662eef
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-04 20:36:23 -0700

    create models categories, and install mongosee and create schema

---
+++ models/categorie.js
@@ -0,0 +1,25 @@
+const mongoose = requier('mongoose');
+const Joi = require('joi');
+
+const categorieSchema = new mongoose.genreSchema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+});
+
+const Categorie = mongoose.model('Cagtegories', categorieSchema);
+
+function validateCategories(categorie) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required()
+    };
+    return Joi.validate(categorie, schema);
+
+}
+
+exports.categorie = Categorie;
+exports.validate = validateCategories;
+exports.categorieSchema = categorieSchema;
