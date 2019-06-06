c6a98d91ccab613bd41f8df6aabfc15ea6f693ed
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-05 21:41:47 -0700

    create some  integration test  categories

--- tests/integration/categories.test.js
+++ tests/integration/categories.test.js
@@ -1,6 +1,15 @@
 const request = require('supertest');
 const { Categorie } = require('../../models/categorie');
+
 let server;
+let nameCategore;
+
+const categories = [
+    { name: 'coffee' },
+    { name: 'te' },
+    { name: 'snack' }
+
+]
 
 describe('/api/categories', () => {
     beforeEach(() => { server = require('../../index') });
@@ -10,15 +19,9 @@ describe('/api/categories', () => {
         await Categorie.deleteMany({});
     });
 
-
     describe('GET / ', () => {
         it('should return all categories ', async () => {
-            const categories = [
-                { name: 'coffee' },
-                { name: 'te' },
-                { name: 'snack' }
 
-            ]
             await Categorie.collection.insertMany(categories, function (error, docus) { });
 
             const res = await request(server).get('/api/categories');
@@ -30,4 +33,28 @@ describe('/api/categories', () => {
             expect(res.body.some(c => c.name === "snack")).toBeTruthy();
         });
     });
+
+    describe('POST', () => {
+
+        const info = () => {
+            return request(server).post('/api/categories').send({ nameCategore });
+        };
+        beforeEach(() => {
+            nameCategore = " Coffee";
+        });
+
+        it('should return error 400 if name is less than 5 characters', async () => {
+            nameCategore = "abcd";
+            const res = await info();
+            expect(res.status).toBe(400);
+        });
+
+        it('should return error 400 if the name is empty', async () => {
+            nameCategore = "";
+            const res = await info();
+            expect(res.status).toBe(400);
+        })
+
+    });
+
 })
