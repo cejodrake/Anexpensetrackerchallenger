692843721b364110dae8b84fdf6bed4bffbc1310
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-06 10:51:45 -0700

    configuration file expenses.test.js for test of validationes

--- tests/integration/expenses.test.js
+++ tests/integration/expenses.test.js
@@ -1,4 +1,27 @@
 const request = require('supertest');
+const mongoose = require('mongoose')
 const { Expense } = require('../../models/expense');
+const app = require('../../index');
 
 
+describe('/api/expenses', () => {
+    let server;
+    let expense;
+    let date;
+    let categorieId;
+    let total;
+
+    const info = () => {
+        return request(server).post('/api/expenses').send({ date, categorieId, total });
+
+    };
+
+    beforeEach(async () => {
+        server = require(app);
+        categorieId = mongoose.Types.ObjectId();
+
+        expense = new Expense({
+
+        })
+    })
+})
