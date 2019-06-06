fe0b7533e4d42c45023317fd6d32376f26567f7d
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-06 11:24:10 -0700

    fix test for validation expenses

--- tests/integration/expenses.test.js
+++ tests/integration/expenses.test.js
@@ -2,26 +2,45 @@ const request = require('supertest');
 const mongoose = require('mongoose')
 const { Expense } = require('../../models/expense');
 const app = require('../../index');
-
+let server;
 
 describe('/api/expenses', () => {
-    let server;
+
     let expense;
-    let date;
+    let dateTest = "2019-06-05";
     let categorieId;
-    let total;
+    let totalTest = 100;
+
+
 
     const info = () => {
-        return request(server).post('/api/expenses').send({ date, categorieId, total });
+        return request(server).post('/api/expenses').send({ dateTest, categorieId, totalTest });
 
     };
 
     beforeEach(async () => {
-        server = require(app);
+        server = require('../../index');
         categorieId = mongoose.Types.ObjectId();
 
         expense = new Expense({
+            date: dateTest,
+            categorie: {
+                _id: categorieId,
+                name: "burger king"
+            },
+            total: totalTest
+        });
+        await expense.save();
+    });
 
-        })
+    afterEach(async () => {
+        await server.close();
+        await Expense.remove({});
     })
-})
+
+    it('should return error 400 if date if not provided from client ', async () => {
+        dateTest = "f";
+        const res = await info();
+        expect(res.status).toBe(400);
+    })
+});
