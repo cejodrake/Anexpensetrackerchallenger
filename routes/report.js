8763c1bf27b8cfef131ebc0fdd0b53989734a994
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-13 20:52:35 -0700

    create test for report

---
+++ routes/report.js
@@ -0,0 +1,12 @@
+const { Expense } = require('../models/expense');
+const asyncMiddleware = require('../middleware/async');
+const { validateInputsExpenses } = require('../helpers/validationes');
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+
+})
+
--- tests/integration/report.test.js
+++ tests/integration/report.test.js
@@ -17,12 +17,20 @@ describe(api, () => {
         );
     }
 
+
     beforeEach(async () => {
         server = require('../../index');
     });
     afterEach(async () => {
         await server.close();
     });
+    // date1 shouldn't be than  less to date now date1 < now.
+    // date1 shouldn't be  greater date2 date1 <=  date2
 
+    it('date1 should be not less than date now', async () => {
+        let dateNow = Date.now();
+        const res = await requesClient();
+        expect(res.status).toBe(400);
 
+    });
 });
