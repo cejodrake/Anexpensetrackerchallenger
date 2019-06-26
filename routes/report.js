a5f49d14e631dbb14ebc4caa7460674826da0d43
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-06-25 22:12:47 -0700

    report fix  consult about group for date for categorie

--- routes/report.js
+++ routes/report.js
@@ -8,63 +8,22 @@ const { validationesFormatDate, validateDateEndLessDateInitial } = require('../h
 const express = require('express');
 const router = express.Router();
 
-const pipe =
-    [
-        {
-
-            $group: {
-                _id: "$categorie",
-                total: { $sum: '$total' },
-            }
-        }
-    ]
-
-
 router.get('/', asyncMiddleware(async (req, res) => {
 
-    let dateInitial = moment(req.body.dateInitial, "YYYY-MM-DD");
-    let dateEnd = moment(req.body.dateEnd, "YYYY-MM-DD");
+    let dateInitial = moment(req.body.dateInitial);
+    let dateEnd = moment(req.body.dateEnd);
 
     if (validateDateEndLessDateInitial(dateInitial, dateEnd))
         return res.status(400).send('Date End is less than Date Initial');
 
     if (!validationesFormatDate(dateInitial, dateEnd))
         return res.status(400).send('Some Field date is not Correct Format');
-    var pipeline = [
-
-        {
-            $match: {
-                "date": {
-                    $gte: dateInitial, $lte: dateEnd
-                }
-            }
-        },
-
-        {
-            $group: {
-                _id: "$categorie",
-                total: { $sum: "$total" }
-            }
-        }
-    ]
-    const result = await Expense.aggregate(pipeline); /*await Expense.aggregate([
-        {
-            $group: {
-                _id: "$categorie",
-                total: { $sum: "$total" }
-            },
-        }
-    ])*/
-
-    console.log(result);
 
     console.log("fecha Inicial ---" + dateInitial + "fecha final --- :" + dateEnd)
 
     const allData = await Expense.find({
         date: { $gte: dateInitial, $lte: dateEnd }
     });
-
-    console.log(dateInitial + dateEnd)
 
 
     return res.status(200).send(allData);
